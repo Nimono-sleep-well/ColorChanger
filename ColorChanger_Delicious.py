2b3373fd4eda7ed61e5272990d4d9568c51c934c
Happy birthday to sus <117884839+Nimono-sleep-well@users.noreply.github.com>
2023-05-01 01:02:34 +0900

    Enter出力

--- ColorChanger_Delicious.py
+++ ColorChanger_Delicious.py
@@ -5,36 +5,41 @@ Created on Wed Apr 26 17:34:08 2023
 @author: monji
 """
 #import
-import tkinter
+import tkinter as tk
+import tkinter.ttk as ttk
 
 
 #tkクラス作成
-tki = tkinter.Tk()
+tki = tk.Tk()
 #画面サイズ
 tki.geometry("800x500")
 #画面タイトル
 tki.title("ColorChanger")
 
-var_R = tkinter.StringVar(tki)
-var_G = tkinter.StringVar(tki)
-var_B = tkinter.StringVar(tki)
+var_R = tk.StringVar(tki)
+var_G = tk.StringVar(tki)
+var_B = tk.StringVar(tki)
+
+def key_event(e):
+    btn_click()
+    return 0
 
 def com(a, b, c):
     Com = [int(255 - a), int(255 - b), int(255 - c)]
     return Com
     
 def reset_click():
-    txt_main.delete(0, tkinter.END)
-    txt_R.delete(0,tkinter.END)
-    txt_G.delete(0,tkinter.END)
-    txt_B.delete(0,tkinter.END)
-    txt_ana1.delete(0,tkinter.END)
-    txt_ana2.delete(0,tkinter.END)
-    txt_ana3.delete(0,tkinter.END)
-    txt_ana4.delete(0,tkinter.END)
-    txt_bri.delete(0,tkinter.END)
-    txt_rev.delete(0,tkinter.END)
-    txt_In.delete(0,tkinter.END)
+    txt_main.delete(0, tk.END)
+    txt_R.delete(0,tk.END)
+    txt_G.delete(0,tk.END)
+    txt_B.delete(0,tk.END)
+    txt_ana1.delete(0,tk.END)
+    txt_ana2.delete(0,tk.END)
+    txt_ana3.delete(0,tk.END)
+    txt_ana4.delete(0,tk.END)
+    txt_bri.delete(0,tk.END)
+    txt_rev.delete(0,tk.END)
+    txt_In.delete(0,tk.END)
     
     lbl_main["bg"] = "#cecfff"
     lbl_ana1["bg"] = "#cecfff"
@@ -57,14 +62,14 @@ def reset_click():
     scale_B.set(0)
         
 def btn_click():
-    txt_ana1.delete(0,tkinter.END)
-    txt_ana2.delete(0,tkinter.END)
-    txt_ana3.delete(0,tkinter.END)
-    txt_ana4.delete(0,tkinter.END)
-    txt_bri.delete(0,tkinter.END)
-    txt_rev.delete(0,tkinter.END)
-    txt_In.delete(0,tkinter.END)
-    txt_main.delete(0,tkinter.END)
+    txt_ana1.delete(0,tk.END)
+    txt_ana2.delete(0,tk.END)
+    txt_ana3.delete(0,tk.END)
+    txt_ana4.delete(0,tk.END)
+    txt_bri.delete(0,tk.END)
+    txt_rev.delete(0,tk.END)
+    txt_In.delete(0,tk.END)
+    txt_main.delete(0,tk.END)
     
     R_input = str(txt_R.get())
     G_input = str(txt_G.get())
@@ -208,9 +213,9 @@ def btn_click():
     txt_rev.insert(0, str(num_rev))
     txt_In.insert(0, str(num_In))
     
-    txt_R.delete(0,tkinter.END)
-    txt_G.delete(0,tkinter.END)
-    txt_B.delete(0,tkinter.END)
+    txt_R.delete(0,tk.END)
+    txt_G.delete(0,tk.END)
+    txt_B.delete(0,tk.END)
     
     lbl_main["bg"] = "#"+str(num_main)
     lbl_ana1["bg"] = "#"+str(num_ana1)
@@ -228,94 +233,101 @@ def btn_click():
     lbl_rev["fg"] = "#"+str(Tom_rev)
     lbl_In["fg"] = "#"+str(Tom_com)
     
+    
+"""
+Enter実行
+"""
+
+tki.bind("<Return>", key_event)
+    
 """
 ラベル
 """
 #入力部
-lbl = tkinter.Label(tki,text = "R", background="#cecfff")
+lbl = tk.Label(tki,text = "R", background="#cecfff")
 lbl.place(x=70, y=45)
-lbl = tkinter.Label(tki,text = "G", background="#cecfff")
+lbl = tk.Label(tki,text = "G", background="#cecfff")
 lbl.place(x=70, y=75)
-lbl = tkinter.Label(tki,text = "B", background="#cecfff")
+lbl = tk.Label(tki,text = "B", background="#cecfff")
 lbl.place(x=70, y=105)
 
-lbl_R = tkinter.Label(tki, textvariable=var_R)
+lbl_R = tk.Label(tki, textvariable=var_R)
 lbl_R.place(x=410, y=45)
-lbl_G = tkinter.Label(tki, textvariable=var_G)
+lbl_G = tk.Label(tki, textvariable=var_G)
 lbl_G.place(x=410, y=75)
-lbl_B = tkinter.Label(tki, textvariable=var_B)
+lbl_B = tk.Label(tki, textvariable=var_B)
 lbl_B.place(x=410, y=105)
 
-scale_R = tkinter.Scale(tki, orient=tkinter.HORIZONTAL, length=150, to=255, showvalue=False, variable=var_R)
+scale_R = tk.Scale(tki, orient=tk.HORIZONTAL, length=150, to=255, showvalue=False, variable=var_R)
 scale_R.place(x=250, y=45)
-scale_G = tkinter.Scale(tki, orient=tkinter.HORIZONTAL, length=150, to=255, showvalue=False, variable=var_G)
+scale_G = tk.Scale(tki, orient=tk.HORIZONTAL, length=150, to=255, showvalue=False, variable=var_G)
 scale_G.place(x=250, y=75)
-scale_B = tkinter.Scale(tki, orient=tkinter.HORIZONTAL, length=150, to=255, showvalue=False, variable=var_B)
+scale_B = tk.Scale(tki, orient=tk.HORIZONTAL, length=150, to=255, showvalue=False, variable=var_B)
 scale_B.place(x=250, y=105)
 #scale
 
 #境界
-lbl = tkinter.Label(tki,text = "-入力--------------", font=("Myrica M", "20", "bold"))
+lbl = tk.Label(tki,text = "-入力--------------", font=("Myrica M", "20", "bold"))
 lbl.place(x=70, y=10)
-lbl = tkinter.Label(tki,text = "-実行結果----------", font=("Myrica M", "20", "bold"))
+lbl = tk.Label(tki,text = "-実行結果----------", font=("Myrica M", "20", "bold"))
 lbl.place(x=70, y=130)
 
 #出力部
-lbl_main =tkinter.Label(tki,text = "Main Color", background="#cecfff", font=("Myrica M", "10", "bold"))
+lbl_main =tk.Label(tki,text = "Main Color", background="#cecfff", font=("Myrica M", "10", "bold"))
 lbl_main.place(x=70, y=170)
-lbl_ana1 = tkinter.Label(tki,text = "類似色Ⅰ", background="#cecfff", font=("Myrica M", "10", "bold"))
+lbl_ana1 = tk.Label(tki,text = "類似色Ⅰ", background="#cecfff", font=("Myrica M", "10", "bold"))
 lbl_ana1.place(x=70, y=230)
-lbl_ana2 = tkinter.Label(tki,text = "類似色Ⅱ", background="#cecfff", font=("Myrica M", "10", "bold"))
+lbl_ana2 = tk.Label(tki,text = "類似色Ⅱ", background="#cecfff", font=("Myrica M", "10", "bold"))
 lbl_ana2.place(x=235, y=230)
-lbl_ana3 = tkinter.Label(tki,text = "類似色Ⅲ", background="#cecfff", font=("Myrica M", "10", "bold"))
+lbl_ana3 = tk.Label(tki,text = "類似色Ⅲ", background="#cecfff", font=("Myrica M", "10", "bold"))
 lbl_ana3.place(x=400, y=230)
-lbl_ana4 = tkinter.Label(tki,text = "類似色Ⅳ", background="#cecfff", font=("Myrica M", "10", "bold"))
+lbl_ana4 = tk.Label(tki,text = "類似色Ⅳ", background="#cecfff", font=("Myrica M", "10", "bold"))
 lbl_ana4.place(x=565, y=230)
 
-lbl_bri = tkinter.Label(tki,text = "輝度", bg="#cecfff", font=("Myrica M", "10", "bold"))
+lbl_bri = tk.Label(tki,text = "輝度", bg="#cecfff", font=("Myrica M", "10", "bold"))
 lbl_bri.place(x=70, y=290)
-lbl_rev = tkinter.Label(tki,text = "反転色", bg="#cecfff", font=("Myrica M", "10", "bold"))
+lbl_rev = tk.Label(tki,text = "反転色", bg="#cecfff", font=("Myrica M", "10", "bold"))
 lbl_rev.place(x=235, y=290)
-lbl_In = tkinter.Label(tki,text = "補色", bg="#cecfff", font=("Myrica M", "10", "bold"))
+lbl_In = tk.Label(tki,text = "補色", bg="#cecfff", font=("Myrica M", "10", "bold"))
 lbl_In.place(x=400, y=290)
 
 """
 テキストボックス
 """
 #入力部
-txt_R = tkinter.Entry(width = 20)
+txt_R = tk.Entry(width = 20)
 txt_R.place(x=100, y=45)
-txt_G = tkinter.Entry(width = 20)
+txt_G = tk.Entry(width = 20)
 txt_G.place(x=100, y=75)
-txt_B = tkinter.Entry(width = 20)
+txt_B = tk.Entry(width = 20)
 txt_B.place(x=100, y=105)
 
 #出力部
-txt_main = tkinter.Entry(width = 20)
+txt_main = tk.Entry(width = 20)
 txt_main.place(x=70, y=200)
-txt_ana1 = tkinter.Entry(width = 20)
+txt_ana1 = tk.Entry(width = 20)
 txt_ana1.place(x=70, y=260)
-txt_ana2 = tkinter.Entry(width = 20)
+txt_ana2 = tk.Entry(width = 20)
 txt_ana2.place(x=235, y=260)
-txt_ana3 = tkinter.Entry(width = 20)
+txt_ana3 = tk.Entry(width = 20)
 txt_ana3.place(x=400, y=260)
-txt_ana4 = tkinter.Entry(width = 20)
+txt_ana4 = tk.Entry(width = 20)
 txt_ana4.place(x=565, y=260)
 
-txt_bri = tkinter.Entry(width = 20)
+txt_bri = tk.Entry(width = 20)
 txt_bri.place(x=70, y=320)
-txt_rev = tkinter.Entry(width = 20)
+txt_rev = tk.Entry(width = 20)
 txt_rev.place(x=235, y=320)
-txt_In = tkinter.Entry(width = 20)
+txt_In = tk.Entry(width = 20)
 txt_In.place(x=400, y=320)
 
 """
 ボタン
 """
-btn = tkinter.Button(tki,text="出力", command=btn_click, bg="#fffd8f")
+btn = tk.Button(tki,text="出力", command=btn_click, bg="#fffd8f")
 btn.place(x=450, y=70)
 
-btn = tkinter.Button(tki,text="Reset", command=reset_click, bg="#7092be", font="10")
+btn = tk.Button(tki,text="Reset", command=reset_click, bg="#7092be", font="10")
 btn.place(x=70, y=360)
 
 tki.mainloop()
